0343152553db58a2580f187cabb40138bc27489e
ray-a135 <rachelauguste36@gmail.com>
2025-04-16 05:55:00 -0700

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,63 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "hio":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . c c c c c . . . . . 
+. . . . . c c c c c c c . . . . 
+. . . . c c 3 3 3 3 c c c . . . 
+. . . c c . 3 c 3 c 3 c c c . . 
+. . c c . . 3 3 3 3 3 c c c . . 
+. . c c . . 3 c c c 3 . . c . . 
+. c c . . . . . f . . . . . f . 
+. c . f f . . . f . . . . f f . 
+. c . . f f f f f . . . f f . . 
+. . . . . . . f f f f f . . . . 
+. . . . . . . . f . . . . . . . 
+. . . . . . . . f . . . . . . . 
+. . . . . . . . f . . . . . . . 
+`;
+            case "image2":
+            case "iop":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . e e . . . . . . . 
+. . . . e e e e e . . . . . . . 
+. . . . e e e e 7 . . . . . . . 
+. . . . 7 7 7 7 7 . . . . . . . 
+. e . . 7 7 7 7 e 3 . . . . e . 
+. e . . 7 e 7 7 7 . . . . e e . 
+. e . . 7 7 7 7 7 . . . e e . . 
+. e . . 7 e e e 7 . . . e . . . 
+. e . . . e e e . . . e e . . . 
+. e e . . e e e . . e e . . . . 
+. . e e . e e e e e e . . . . . 
+. . . e e e e e e . . . . . . . 
+. . . . . e e e . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
---
+++ tilemap.g.ts
@@ -0,0 +1,39 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`1000100001010101020101020101020102010301010101020201010201010201010303010102010201010102010102010303010101020102010101020101020103030101010201020101010201010203030201010102010201010102010103030302010104020102010101020101030301020101040201020201010201010303010201010402010102010102010303020102010101040101020101020101010201020101010401010201010303030302010201010204010102010303030303020102010102040201020103030303030102020101020402010201030303030303020201010202040102010303030303030201010101020404010101030303030301010101`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,sprites.castle.tileGrass2,sprites.builtin.forestTiles0,sprites.castle.tilePath4,sprites.castle.tileDarkGrass2], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
